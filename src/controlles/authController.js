b7af7f41f7a622c505d7f3026eaf99b7ed088f71
Paulo <pc5783346@gmail.com>
2022-06-30 16:19:34 -0300

    fix: auth routes now respond with error status 500 on failure

--- src/controlles/authController.js
+++ src/controlles/authController.js
@@ -16,16 +16,21 @@ export async function signUp (req, res){
     if(value.error){
         res.sendStatus(422);
     }else{
-        const userDoc = await db.collection("users").findOne({ email: userData.email });
-        if(userDoc) return res.sendStatus(409);
+        try{
+            const userDoc = await db.collection("users").findOne({ email: userData.email });
+            if(userDoc) return res.sendStatus(409);
 
-        const passwordHash = bcrypt.hashSync(userData.password, 10);
-        delete userData.passwordconfirm;
-        await db.collection("users").insertOne({
-            ...userData,
-            password: passwordHash
-        });
-        res.sendStatus(201);
+            const passwordHash = bcrypt.hashSync(userData.password, 10);
+            delete userData.passwordconfirm;
+            await db.collection("users").insertOne({
+                ...userData,
+                password: passwordHash
+            });
+
+            res.sendStatus(201);
+        }catch(e){
+            res.status(500).send(e);
+        }
     }
 }
 
@@ -38,10 +43,17 @@ export async function signIn (req, res){
     });
     if(user && bcrypt.compareSync(password, user.password)){
         const token = uuid();
-        await db.collection("sessions").insertOne({ userId: user._id, token });
-        delete user.password
-        const re = { ...user, token }
-        res.status(200).send(re);
+        db.collection("sessions").deleteMany({userId: user._id}).then(()=>
+            db.collection("sessions").insertOne({ userId: user._id, token }).then(() => {
+                delete user.password;
+                const re = { ...user, token };
+                res.status(200).send(re);
+            }).catch((e) =>
+                res.status(500).send(e)   
+            )
+        ).catch((e) =>
+            res.status(500).send(e)
+        );
     }else{
         res.sendStatus(401);
     }
