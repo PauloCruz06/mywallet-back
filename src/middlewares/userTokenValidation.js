a3854e4ffa284184dc2c87a1ead923c6584c5550
Paulo <pc5783346@gmail.com>
2022-07-01 14:05:54 -0300

    feat: creation of middleware that validates the user's token

---
+++ src/middlewares/userTokenValidation.js
@@ -0,0 +1,22 @@
+import db from "../db.js";
+
+export default async function tokenValidation(req, res, next){
+    const { Authorization } = req.headers;
+    const token = Authorization?.replace('Bearer', '');
+    if(token && typeof token === 'string'){
+        try{
+            const userSession = await db.collection("sessions").findOne({ token });
+            if(!userSession) return res.sendStatus(401);
+
+            const user = await db.collection("users").findOne({ _id: userSession.userId });
+            if(!user) return res.sendStatus(404);
+
+            res.locals.user = user;
+            next();
+        }catch(e){
+            res.status(500).send(e);
+        }
+    }else{
+        res.sendStatus(401);
+    }
+}
