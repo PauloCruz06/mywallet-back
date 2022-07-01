c4ca13046b58a950a08506b253e1d3bc0a9baea1
Paulo <pc5783346@gmail.com>
2022-06-30 23:16:27 -0300

    style: route functions are now organized in routes folder

--- src/controlles/authController.js
+++ src/controlles/authController.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import db from "../db.js";
 import { v4 as uuid } from "uuid";
 import { stripHtml } from "string-strip-html";
-import { validationSignUp } from "../validation.js";
+import { validationSignUp } from "../schemaValidations/validation.js";
 
 export async function signUp (req, res){
     try{
@@ -14,6 +14,7 @@ export async function signUp (req, res){
         };
     
         const value = await validationSignUp(userData);
+
         if(value.error){
             res.sendStatus(422);
         }else{
@@ -39,17 +40,19 @@ export async function signUp (req, res){
 }
 
 export async function signIn (req, res){
-    const { email, password} = req.body;
+    const { email, password } = req.body;
     if(!email || !password) return res.sendStatus(422);
 
     const user = await db.collection("users").findOne({
         email: stripHtml(email).result.trim()
     });
+
     if(user && bcrypt.compareSync(password, user.password)){
         const token = uuid();
         db.collection("sessions").deleteMany({userId: user._id}).then(()=>
             db.collection("sessions").insertOne({ userId: user._id, token }).then(() => {
                 delete user.password;
+                delete user._id;
                 const re = { ...user, token };
                 res.status(200).send(re);
             }).catch((e) =>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { signIn, signUp } from "./controlles/authController.js";
+import authRouter from "./routes/authRouter.js";
 
 dotenv.config();
 
 const server = express();
+
 server.use(express.json());
 server.use(cors());
-
-server.post("/sign-up", signUp);
-server.post("/sign-in", signIn);
+server.use(authRouter);
 
 const PORT = process.env.PORT;
+
 server.listen(PORT, () => console.log("Server is listening on port."));
